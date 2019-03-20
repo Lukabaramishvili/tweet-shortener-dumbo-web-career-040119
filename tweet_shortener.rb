8b4281448f7f53a584b773257b20e206d5ccf684
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-20 15:36:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ def dictionary
  def word_substituter(first_tweet)
   compared = first_tweet.split(" ")
   compared.each do |word|
-    if word.include?()
+    if dictionary.key.include?(word)
  
  
  
