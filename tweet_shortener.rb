70b06a5c0af6b33fb38aa7befc186cb1085d8155
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-20 15:25:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,5 +2,14 @@
 
 def dictionary
   hash = { 
-          "hello": "hi" ,
+    "hello": "hi" ,
+    "to": "2",
+    "two": "2",
+    "too": "2",
+    "for": "4",
+    "four": "4",
+    "be": "b",
+    "you": "u",
+    "at": "@",
+    "and": "&"
             }
